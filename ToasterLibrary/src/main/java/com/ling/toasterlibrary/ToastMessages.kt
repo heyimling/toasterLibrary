946c82ee18650fe3a4bd1f975af03bb8d7dcbe94
Wei-Ling Wu <weiling.wu@Aditis-MacBook-Pro.local>
2022-09-13 13:12:29 -0700

    Add a fixedToast func

--- ToasterLibrary/src/main/java/com/ling/toasterlibrary/ToastMessages.kt
+++ ToasterLibrary/src/main/java/com/ling/toasterlibrary/ToastMessages.kt
@@ -7,4 +7,8 @@ class ToastMessages {
     fun toast(c: Context, msg: String) {
         Toast.makeText(c, msg, Toast.LENGTH_LONG).show()
     }
+
+    fun fixedToast(c: Context) {
+        Toast.makeText(c, "This is a new function", Toast.LENGTH_LONG).show()
+    }
 }
